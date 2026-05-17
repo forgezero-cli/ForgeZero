bcf6a0b0f09b625b1c3c766650d161333924e688
alexvoste <meshoff@proton.me>
2026-05-17 21:35:12 +0300

    feat(linker): add CmdRunner interface for testable command execution

---
+++ internal/linker/runner.go
@@ -0,0 +1,17 @@
+package linker
+
+import (
+	"context"
+
+	"fz/internal/utils"
+)
+
+type CmdRunner interface {
+	Run(ctx context.Context, verbose bool, name string, args ...string) (output string, err error)
+}
+
+type RealCmdRunner struct{}
+
+func (r *RealCmdRunner) Run(ctx context.Context, verbose bool, name string, args ...string) (string, error) {
+	return utils.RunCommandSilent(ctx, verbose, name, args...)
+}
