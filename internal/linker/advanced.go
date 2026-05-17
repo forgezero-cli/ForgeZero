d2fd93da5d00ccf67f795e6b430f05b4e62f7744
alexvoste <meshoff@proton.me>
2026-05-18 01:27:02 +0300

    feat(linker): add helper functions for linker scripts, text address, format validation

---
+++ internal/linker/advanced.go
@@ -0,0 +1,47 @@
+package linker
+
+import (
+	"fmt"
+
+	"fz/internal/assembler"
+)
+
+func ApplyGccLdFlags(args []string, ldScript, textAddr string) []string {
+	if ldScript != "" {
+		args = append(args, "-Wl,-T,"+ldScript)
+	}
+	if textAddr != "" {
+		args = append(args, "-Wl,-Ttext="+textAddr)
+	}
+	return args
+}
+
+func ApplyLdFlags(args []string, ldScript, textAddr string) []string {
+	if ldScript != "" {
+		args = append(args, "-T", ldScript)
+	}
+	if textAddr != "" {
+		args = append(args, "-Ttext", textAddr)
+	}
+	return args
+}
+
+func SetOutputFormat(format string) error {
+	valid := map[string]bool{"elf32": true, "elf64": true, "bin": true}
+	if !valid[format] {
+		return fmt.Errorf("invalid output format: %s (supported: elf32, elf64, bin)", format)
+	}
+	assembler.OutputFormat = format
+	return nil
+}
+
+func CompilerForExtension(ext string) string {
+	switch ext {
+	case ".c":
+		return "gcc"
+	case ".cpp", ".cc", ".cxx", ".c++":
+		return "g++"
+	default:
+		return "gcc"
+	}
+}
