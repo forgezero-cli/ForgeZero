b7b9c3f100ad884c4d4d039addfbe2f1aa3bfb20
alexvoste <meshoff@proton.me>
2026-05-18 01:39:02 +0300

    feat(shell): add BuildState and default state for interactive session

---
+++ internal/shell/state.go
@@ -0,0 +1,37 @@
+package shell
+
+type BuildState struct {
+	Mode          string
+	Format        string
+	Strict        bool
+	Sanitize      bool
+	Verbose       bool
+	Debug         bool
+	NoCache       bool
+	NoSymbolCheck bool
+	KeepObj       bool
+	LdScript      string
+	TextAddr      string
+	Out           string
+	SourcePath    string
+	SourceType    string // "asm", "cc", "dir"
+}
+
+func DefaultState() *BuildState {
+	return &BuildState{
+		Mode:          "auto",
+		Format:        "elf64",
+		Strict:        false,
+		Sanitize:      true,
+		Verbose:       false,
+		Debug:         false,
+		NoCache:       false,
+		NoSymbolCheck: false,
+		KeepObj:       false,
+		LdScript:      "",
+		TextAddr:      "",
+		Out:           "",
+		SourcePath:    ".",
+		SourceType:    "dir",
+	}
+}
