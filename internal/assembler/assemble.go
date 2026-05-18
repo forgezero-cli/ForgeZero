f653553541d14824d98c009e1ae756dad37a4a07
alexvoste <meshoff@proton.me>
2026-05-18 23:50:36 +0300

    feat(assembler): add cross-compilation support via -target (ARM, RISC-V)

--- internal/assembler/assemble.go
+++ internal/assembler/assemble.go
@@ -9,18 +9,54 @@ import (
 	"fz/internal/utils"
 )
 
-var OutputFormat = "elf64"
+var (
+	OutputFormat = "elf64"
+	Target       = "x86_64-linux-gnu"
+)
 
-func formatToNasmFlag(format string) string {
-	switch format {
-	case "elf32":
-		return "-felf32"
-	case "elf64":
+func formatForTarget() string {
+	switch {
+	case strings.Contains(Target, "x86_64"):
 		return "-felf64"
-	case "bin":
-		return "-fbin"
+	case strings.Contains(Target, "i386") || strings.Contains(Target, "i686"):
+		return "-felf32"
+	case strings.Contains(Target, "arm"):
+		return "-march=armv7-a"
 	default:
 		return "-felf64"
+	}
+}
+
+func gasCmdForTarget() string {
+	switch {
+	case strings.Contains(Target, "arm"):
+		return "arm-linux-gnueabihf-as"
+	case strings.Contains(Target, "riscv"):
+		return "riscv64-unknown-elf-as"
+	default:
+		return "as"
+	}
+}
+
+func ccForTarget() string {
+	switch {
+	case strings.Contains(Target, "arm"):
+		return "arm-linux-gnueabihf-gcc"
+	case strings.Contains(Target, "riscv"):
+		return "riscv64-unknown-elf-gcc"
+	default:
+		return "gcc"
+	}
+}
+
+func cxxForTarget() string {
+	switch {
+	case strings.Contains(Target, "arm"):
+		return "arm-linux-gnueabihf-g++"
+	case strings.Contains(Target, "riscv"):
+		return "riscv64-unknown-elf-g++"
+	default:
+		return "g++"
 	}
 }
 
@@ -40,7 +76,7 @@ func Assemble(ctx context.Context, src, obj string, debug, verbose bool, mode st
 		}
 		return assembleNASM(ctx, src, obj, debug, verbose)
 	case ".s", ".S":
-		if err := utils.CheckTool("gcc"); err != nil {
+		if err := utils.CheckTool(gasCmdForTarget()); err != nil {
 			return err
 		}
 		return assembleGAS(ctx, src, obj, debug, verbose)
@@ -50,12 +86,12 @@ func Assemble(ctx context.Context, src, obj string, debug, verbose bool, mode st
 		}
 		return assembleFASM(ctx, src, obj, verbose)
 	case ".c":
-		if err := utils.CheckTool("gcc"); err != nil {
+		if err := utils.CheckTool(ccForTarget()); err != nil {
 			return err
 		}
 		return assembleC(ctx, src, obj, debug, verbose)
 	case ".cpp", ".cc", ".cxx", ".c++":
-		if err := utils.CheckTool("g++"); err != nil {
+		if err := utils.CheckTool(cxxForTarget()); err != nil {
 			return err
 		}
 		return assembleCpp(ctx, src, obj, debug, verbose)
@@ -65,8 +101,8 @@ func Assemble(ctx context.Context, src, obj string, debug, verbose bool, mode st
 }
 
 func assembleNASM(ctx context.Context, src, obj string, debug, verbose bool) error {
-	formatFlag := formatToNasmFlag(OutputFormat)
-	args := []string{formatFlag, src, "-o", obj}
+	format := formatForTarget()
+	args := []string{format, src, "-o", obj}
 	if debug {
 		args = append([]string{"-g"}, args...)
 	}
@@ -84,19 +120,20 @@ func assembleNASM(ctx context.Context, src, obj string, debug, verbose bool) err
 }
 
 func assembleGAS(ctx context.Context, src, obj string, debug, verbose bool) error {
+	cmd := gasCmdForTarget()
 	args := []string{"-c", src, "-o", obj}
 	if debug {
 		args = append([]string{"-g"}, args...)
 	}
 	if verbose {
-		fmt.Println("Running: gcc", strings.Join(args, " "))
+		fmt.Printf("Running: %s %s\n", cmd, strings.Join(args, " "))
 	}
-	output, err := utils.RunCommandSilent(ctx, verbose, "gcc", args...)
+	output, err := utils.RunCommandSilent(ctx, verbose, cmd, args...)
 	if err != nil {
 		if !verbose {
-			return fmt.Errorf("gcc assembly failed (use -verbose for details)")
+			return fmt.Errorf("%s failed (use -verbose for details)", cmd)
 		}
-		return fmt.Errorf("gcc failed: %w\n%s", err, output)
+		return fmt.Errorf("%s failed: %w\n%s", cmd, err, output)
 	}
 	return nil
 }
@@ -117,6 +154,7 @@ func assembleFASM(ctx context.Context, src, obj string, verbose bool) error {
 }
 
 func assembleC(ctx context.Context, src, obj string, debug, verbose bool) error {
+	compiler := ccForTarget()
 	args := []string{"-c", src, "-o", obj}
 	strictFlags := []string{"-Wall", "-Wextra", "-Werror", "-Wpedantic", "-Wshadow", "-Wconversion"}
 	args = append(args, strictFlags...)
@@ -124,19 +162,20 @@ func assembleC(ctx context.Context, src, obj string, debug, verbose bool) error 
 		args = append(args, "-g")
 	}
 	if verbose {
-		fmt.Println("Running: gcc", strings.Join(args, " "))
+		fmt.Printf("Running: %s %s\n", compiler, strings.Join(args, " "))
 	}
-	output, err := utils.RunCommandSilent(ctx, verbose, "gcc", args...)
+	output, err := utils.RunCommandSilent(ctx, verbose, compiler, args...)
 	if err != nil {
 		if !verbose {
-			return fmt.Errorf("gcc compilation failed (use -verbose for details)")
+			return fmt.Errorf("%s compilation failed (use -verbose for details)", compiler)
 		}
-		return fmt.Errorf("gcc failed: %w\n%s", err, output)
+		return fmt.Errorf("%s failed: %w\n%s", compiler, err, output)
 	}
 	return nil
 }
 
 func assembleCpp(ctx context.Context, src, obj string, debug, verbose bool) error {
+	compiler := cxxForTarget()
 	args := []string{"-c", src, "-o", obj}
 	strictFlags := []string{"-Wall", "-Wextra", "-Werror", "-Wpedantic", "-Wshadow", "-Wconversion"}
 	args = append(args, strictFlags...)
@@ -144,14 +183,14 @@ func assembleCpp(ctx context.Context, src, obj string, debug, verbose bool) erro
 		args = append(args, "-g")
 	}
 	if verbose {
-		fmt.Println("Running: g++", strings.Join(args, " "))
+		fmt.Printf("Running: %s %s\n", compiler, strings.Join(args, " "))
 	}
-	output, err := utils.RunCommandSilent(ctx, verbose, "g++", args...)
+	output, err := utils.RunCommandSilent(ctx, verbose, compiler, args...)
 	if err != nil {
 		if !verbose {
-			return fmt.Errorf("g++ compilation failed (use -verbose for details)")
+			return fmt.Errorf("%s compilation failed (use -verbose for details)", compiler)
 		}
-		return fmt.Errorf("g++ failed: %w\n%s", err, output)
+		return fmt.Errorf("%s failed: %w\n%s", compiler, err, output)
 	}
 	return nil
 }
