0042a34f911026540d1278084404e652f8eefa3d
alexvoste <meshoff@proton.me>
2026-05-22 00:41:01 +0300

    feat(io): implement unix-specific syscall.Mmap for zero-copy access

---
+++ internal/utils/mmap_unix.go
@@ -0,0 +1,44 @@
+//go:build unix
+
+package utils
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+func mmapFile(fd int, size int64) ([]byte, error) {
+	sizeInt := int(size)
+	data, err := syscall.Mmap(fd, 0, sizeInt, syscall.PROT_READ, syscall.MAP_SHARED)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func unmapFile(data []byte) error {
+	if len(data) == 0 {
+		return nil
+	}
+	return syscall.Munmap(data)
+}
+
+func madviseNormal(data []byte) {
+	if len(data) > 0 {
+		syscall.Madvise(data, syscall.MADV_NORMAL)
+	}
+}
+
+func getFileDescriptor(f interface {
+	Fd() uintptr
+}) int {
+	return int(f.Fd())
+}
+
+func unsafeByteSlice(ptr unsafe.Pointer, len int) []byte {
+	return *(*[]byte)(unsafe.Pointer(&struct {
+		data uintptr
+		len  int
+		cap  int
+	}{uintptr(ptr), len, len}))
+}
