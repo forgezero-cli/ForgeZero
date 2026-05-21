6e9717815bb5ea1de45fb47f443ed6bbbe77d047
alexvoste <meshoff@proton.me>
2026-05-21 18:41:24 +0300

    feat(fs): extend filesystem interface with verified open, symlink resolution, and SameFile validation

--- internal/fs/fs.go
+++ internal/fs/fs.go
@@ -1,8 +1,18 @@
 package fs
 
 import (
+	"errors"
 	"io"
 	"os"
+)
+
+var (
+	ErrDiskFull      = errors.New("disk full")
+	ErrPermission    = errors.New("permission denied")
+	ErrTimeout       = errors.New("i/o timeout")
+	ErrInterrupted   = errors.New("interrupted system call")
+	ErrSymlink       = errors.New("symlink not permitted")
+	ErrPathChanged   = errors.New("path changed during open")
 )
 
 type FileSystem interface {
@@ -10,6 +20,7 @@ type FileSystem interface {
 	WriteFile(path string, data []byte, perm os.FileMode) error
 	ReadFile(path string) ([]byte, error)
 	Open(path string) (io.ReadCloser, error)
+	OpenVerified(path string) (io.ReadCloser, error)
 	CreateTemp(dir, pattern string) (*os.File, error)
 	Remove(name string) error
 	RemoveAll(path string) error
@@ -18,6 +29,9 @@ type FileSystem interface {
 	Lstat(name string) (os.FileInfo, error)
 	ReadDir(name string) ([]os.DirEntry, error)
 	Chmod(name string, mode os.FileMode) error
+	Readlink(name string) (string, error)
+	EvalSymlinks(path string) (string, error)
+	SameFile(a, b os.FileInfo) bool
 }
 
-var Default FileSystem = &Unix{}
+var Default FileSystem = Unix{}
