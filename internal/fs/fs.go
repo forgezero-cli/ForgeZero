51f40afe0d0af22d8429560c49fe74410cfa0cff
alexvoste <meshoff@proton.me>
2026-05-22 16:36:03 +0300

    refactor(fs): implement capability-based VFS interface

--- internal/fs/fs.go
+++ internal/fs/fs.go
@@ -4,7 +4,62 @@ import (
 	"errors"
 	"io"
 	"os"
+	"sync"
+	"sync/atomic"
 )
+
+var isolationMode atomic.Value
+
+func init() {
+	isolationMode.Store("none")
+}
+
+func SetIsolationMode(mode string) {
+	switch mode {
+	case "none", "standard", "strict":
+		isolationMode.Store(mode)
+	default:
+		isolationMode.Store("none")
+	}
+}
+
+func GetIsolationMode() string {
+	v := isolationMode.Load()
+	if v == nil {
+		return "none"
+	}
+	return v.(string)
+}
+
+func IsStrictIsolation() bool {
+	return GetIsolationMode() == "strict"
+}
+
+var readBufferPool = sync.Pool{New: func() any { return make([]byte, 32*1024) }}
+
+func readFileBytes(path string) ([]byte, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	buf := readBufferPool.Get().([]byte)
+	defer readBufferPool.Put(buf)
+	var result []byte
+	for {
+		n, err := f.Read(buf)
+		if n > 0 {
+			result = append(result, buf[:n]...)
+		}
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+	}
+	return result, nil
+}
 
 var (
 	ErrDiskFull    = errors.New("disk full")
