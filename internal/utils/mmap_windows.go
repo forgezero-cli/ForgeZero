df5dc41f3fe75889f26eb734553d6ac905b87e8b
alexvoste <meshoff@proton.me>
2026-05-22 16:36:03 +0300

    feat(utils/mmap): atomic LockFileEx integration and uintptr fix

--- internal/utils/mmap_windows.go
+++ internal/utils/mmap_windows.go
@@ -7,6 +7,11 @@ import (
 	"unsafe"
 )
 
+const (
+	pageReadOnly = 0x02
+	fileMapRead  = 4
+)
+
 var (
 	kernel32    = syscall.NewLazyDLL("kernel32.dll")
 	mapFile     = kernel32.NewProc("MapViewOfFile")
@@ -16,20 +21,32 @@ var (
 )
 
 func mmapFile(fd int, size int64) ([]byte, error) {
+	if size <= 0 {
+		return nil, nil
+	}
 	hFile := syscall.Handle(uintptr(fd))
+	high := uintptr(uint32(uint64(size) >> 32))
+	low := uintptr(uint32(uint64(size)))
 	hMapping, _, err := mapHandle.Call(
 		uintptr(hFile),
 		0,
-		uintptr(4),
-		uint32(size>>32),
-		uint32(size),
+		uintptr(pageReadOnly),
+		high,
+		low,
+		0,
 	)
 	if hMapping == 0 {
 		return nil, syscall.Errno(err.(syscall.Errno))
 	}
 	defer closeHandle.Call(hMapping)
 
-	ptr, _, err := mapFile.Call(hMapping, 0, 0, uint32(size>>32), uint32(size))
+	ptr, _, err := mapFile.Call(
+		uintptr(hMapping),
+		uintptr(fileMapRead),
+		0,
+		0,
+		uintptr(size),
+	)
 	if ptr == 0 {
 		return nil, syscall.Errno(err.(syscall.Errno))
 	}
@@ -47,6 +64,22 @@ func unmapFile(data []byte) error {
 		return err
 	}
 	return nil
+}
+
+func lockFileShared(fd int) error {
+	h := syscall.Handle(uintptr(fd))
+	var ol syscall.Overlapped
+	const lockRangeLow = 0xffffffff
+	const lockRangeHigh = 0xffffffff
+	return syscall.LockFileEx(h, 0, 0, lockRangeLow, lockRangeHigh, &ol)
+}
+
+func unlockFile(fd int) error {
+	h := syscall.Handle(uintptr(fd))
+	var ol syscall.Overlapped
+	const lockRangeLow = 0xffffffff
+	const lockRangeHigh = 0xffffffff
+	return syscall.UnlockFileEx(h, 0, lockRangeLow, lockRangeHigh, &ol)
 }
 
 func madviseNormal(data []byte) {
