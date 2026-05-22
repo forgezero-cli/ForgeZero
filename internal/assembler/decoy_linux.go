09b9e402e8dcf0c76cf27b66f9460575560ab062
alexvoste <meshoff@proton.me>
2026-05-22 23:48:04 +0300

    asm: add platform-specific decoys for hot-path stability

--- internal/assembler/decoy_linux.go
+++ internal/assembler/decoy_linux.go
@@ -3,29 +3,29 @@
 package assembler
 
 import (
-    "os"
-    "syscall"
+	"os"
+	"syscall"
 )
 
 func emitDecoyObject(path string) error {
-    const size = 4096
-    buf := make([]byte, size)
-    copy(buf[:4], []byte{0x7f, 'E', 'L', 'F'})
-    for i := 4; i < size; i++ {
-        buf[i] = byte((i*31)^0xAA)
-    }
-    f, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR|os.O_TRUNC|syscall.O_CLOEXEC, 0o600)
-    if err != nil {
-        return err
-    }
-    defer f.Close()
-    if err := f.Truncate(int64(size)); err != nil {
-        return err
-    }
-    data, err := syscall.Mmap(int(f.Fd()), 0, size, syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED)
-    if err != nil {
-        return err
-    }
-    copy(data, buf)
-    return syscall.Munmap(data)
+	const size = 4096
+	buf := make([]byte, size)
+	copy(buf[:4], []byte{0x7f, 'E', 'L', 'F'})
+	for i := 4; i < size; i++ {
+		buf[i] = byte((i * 31) ^ 0xAA)
+	}
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR|os.O_TRUNC|syscall.O_CLOEXEC, 0o600)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	if err := f.Truncate(int64(size)); err != nil {
+		return err
+	}
+	data, err := syscall.Mmap(int(f.Fd()), 0, size, syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED)
+	if err != nil {
+		return err
+	}
+	copy(data, buf)
+	return syscall.Munmap(data)
 }
