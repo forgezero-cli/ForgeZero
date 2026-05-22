5bcff8d5b7203277ce648c4743d4acab95407cdd
alexvoste <meshoff@proton.me>
2026-05-22 17:30:45 +0300

    test(gloria): validate zero-copy emission

--- internal/gloria/emitter_test.go
+++ internal/gloria/emitter_test.go
@@ -1,23 +1,23 @@
 package gloria
 
 import (
-    "testing"
+	"testing"
 
-    "fz/internal/utils"
+	"fz/internal/utils"
 )
 
 func TestEmitAndExecMain(t *testing.T) {
-    src := `fn main() { @rax = 10; @rbx = 32; @rax += @rbx; return @rax }`
-    code, err := Emit(src)
-    if err != nil {
-        t.Fatalf("emit error: %v", err)
-    }
-    if len(code) == 0 {
-        t.Fatalf("no code emitted")
-    }
-    t.Logf("code: %x", code)
-    out := utils.ExecRawRet(code)
-    if out != 42 {
-        t.Fatalf("unexpected return: %d", out)
-    }
+	src := `fn main() { @rax = 10; @rbx = 32; @rax += @rbx; return @rax }`
+	code, err := Emit(src)
+	if err != nil {
+		t.Fatalf("emit error: %v", err)
+	}
+	if len(code) == 0 {
+		t.Fatalf("no code emitted")
+	}
+	t.Logf("code: %x", code)
+	out := utils.ExecRawRet(code)
+	if out != 42 {
+		t.Fatalf("unexpected return: %d", out)
+	}
 }
