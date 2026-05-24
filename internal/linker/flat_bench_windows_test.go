1920dfff966c6147e8c7d9a91bb5cee131b3ad6d
alexvoste <meshoff@proton.me>
2026-05-24 16:02:07 +0000

    test(linker): add windows-specific zero-allocation benchmarks

---
+++ internal/linker/flat_bench_windows_test.go
@@ -0,0 +1,55 @@
+//go:build windows
+// +build windows
+
+package linker
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"syscall"
+	"testing"
+)
+
+func TestCopyFileHotAllocations(t *testing.T) {
+	dir, err := ioutil.TempDir("", "fz_test_copy")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+	src := filepath.Join(dir, "src.bin")
+	dst := filepath.Join(dir, "dst.bin")
+	data := make([]byte, 1024)
+	if err := ioutil.WriteFile(src, data, 0o644); err != nil {
+		t.Fatal(err)
+	}
+	f := func() {
+		_ = copyFileHot(src, dst)
+		_ = syscall.DeleteFile(dst)
+	}
+	allocs := testing.AllocsPerRun(20, f)
+	if allocs > 0 {
+		t.Fatalf("copyFileHot allocs = %v > 0", allocs)
+	}
+}
+
+func BenchmarkCopyFileHot(b *testing.B) {
+	dir, err := ioutil.TempDir("", "fz_bench_copy")
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+	src := filepath.Join(dir, "src.bin")
+	dst := filepath.Join(dir, "dst.bin")
+	data := make([]byte, 1024*8)
+	if err := ioutil.WriteFile(src, data, 0o644); err != nil {
+		b.Fatal(err)
+	}
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		if err := copyFileHot(src, dst); err != nil {
+			b.Fatal(err)
+		}
+		_ = syscall.DeleteFile(dst)
+	}
+}
