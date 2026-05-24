7c4c0d32433c2db358cceb35aab9a40ec68cf64b
alexvoste <meshoff@proton.me>
2026-05-24 16:01:50 +0000

    perf(linker): linux syscall implementation with stack-buffered path conversion

--- internal/linker/flat.go
+++ internal/linker/flat.go
@@ -1,8 +1,12 @@
+//go:build !windows
+// +build !windows
+
 package linker
 
 import (
 	"context"
 	"syscall"
+	"unsafe"
 
 	"fz/internal/assembler"
 	"fz/internal/utils"
@@ -19,21 +23,21 @@ func linkFlatBinary(ctx context.Context, obj, bin string) error {
 }
 
 func copyFileHot(src, dst string) error {
-	sfd, err := syscall.Open(src, syscall.O_RDONLY, 0)
+	sfd, err := openHot(src, syscall.O_RDONLY, 0)
 	if err != nil {
 		return err
 	}
-	defer syscall.Close(sfd)
 	var st syscall.Stat_t
 	if err := syscall.Fstat(sfd, &st); err != nil {
+		_ = syscall.Close(sfd)
 		return err
 	}
 	mode := uint32(st.Mode & 0777)
-	dfd, err := syscall.Open(dst, syscall.O_WRONLY|syscall.O_CREAT|syscall.O_TRUNC, mode)
+	dfd, err := openHot(dst, syscall.O_WRONLY|syscall.O_CREAT|syscall.O_TRUNC, mode)
 	if err != nil {
+		_ = syscall.Close(sfd)
 		return err
 	}
-	defer syscall.Close(dfd)
 	var buf [65536]byte
 	for {
 		rn, rerr := syscall.Read(sfd, buf[:])
@@ -42,6 +46,8 @@ func copyFileHot(src, dst string) error {
 			for written < rn {
 				wn, werr := syscall.Write(dfd, buf[written:rn])
 				if werr != nil {
+					_ = syscall.Close(dfd)
+					_ = syscall.Close(sfd)
 					return werr
 				}
 				written += wn
@@ -51,11 +57,50 @@ func copyFileHot(src, dst string) error {
 			if rerr == syscall.EINTR {
 				continue
 			}
+			_ = syscall.Close(dfd)
+			_ = syscall.Close(sfd)
 			return rerr
 		}
 		if rn == 0 {
 			break
 		}
+	}
+	if err := syscall.Close(dfd); err != nil {
+		_ = syscall.Close(sfd)
+		return err
+	}
+	if err := syscall.Close(sfd); err != nil {
+		return err
+	}
+	return nil
+}
+
+const atFDCWD = ^uintptr(99)
+
+func openHot(path string, flags int, perm uint32) (int, error) {
+	var buf [4096]byte
+	n := copy(buf[:], path)
+	if n >= len(buf) {
+		return -1, syscall.ENAMETOOLONG
+	}
+	buf[n] = 0
+	fd, _, errno := syscall.Syscall6(syscall.SYS_OPENAT, atFDCWD, uintptr(unsafe.Pointer(&buf[0])), uintptr(flags), uintptr(perm), 0, 0)
+	if errno != 0 {
+		return -1, errno
+	}
+	return int(fd), nil
+}
+
+func unlinkHot(path string) error {
+	var buf [4096]byte
+	n := copy(buf[:], path)
+	if n >= len(buf) {
+		return syscall.ENAMETOOLONG
+	}
+	buf[n] = 0
+	_, _, errno := syscall.Syscall(syscall.SYS_UNLINKAT, atFDCWD, uintptr(unsafe.Pointer(&buf[0])), 0)
+	if errno != 0 {
+		return errno
 	}
 	return nil
 }
