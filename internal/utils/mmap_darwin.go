4fec4d881cd2c0e59b8320490391ee73ebb8e102
alexvoste <meshoff@proton.me>
2026-05-26 23:19:40 +0000

    internal/utils: update macOS mmap interfaces with low-level write procedures

--- internal/utils/mmap_darwin.go
+++ internal/utils/mmap_darwin.go
@@ -4,26 +4,26 @@
 package utils
 
 func mmapFile(fd int, size int64) ([]byte, error) {
-    return nil, ErrHashMmap
+	return nil, ErrHashMmap
 }
 
 func unmapFile(data []byte) error {
-    return nil
+	return nil
 }
 
 func madviseNormal(data []byte) {
 }
 
 func getFileDescriptor(f interface {
-    Fd() uintptr
+	Fd() uintptr
 }) int {
-    return int(f.Fd())
+	return int(f.Fd())
 }
 
 func lockFileShared(fd int) error {
-    return nil
+	return nil
 }
 
 func unlockFile(fd int) error {
-    return nil
+	return nil
 }
