9ff847b759b13df049f93b029cce5b79403b6f4f
alexvoste <meshoff@proton.me>
2026-05-26 23:19:40 +0000

    internal/linker: implement co-located on-disk symbol cache to bypass nm utility execution

--- internal/linker/symbols.go
+++ internal/linker/symbols.go
@@ -2,10 +2,15 @@ package linker
 
 import (
 	"bufio"
+	"bytes"
 	"context"
 	"fmt"
+	"os"
 	"os/exec"
+	"path/filepath"
+	"strconv"
 	"strings"
+	"sync"
 
 	"fz/internal/utils"
 )
@@ -18,6 +23,24 @@ type SymbolInfo struct {
 	Bound string
 }
 
+var (
+	detectedTool     string
+	detectedToolOnce sync.Once
+)
+
+func getSymbolsTool() string {
+	detectedToolOnce.Do(func() {
+		if _, err := exec.LookPath("nm"); err != nil {
+			detectedTool = "nm"
+		} else if _, err := exec.LookPath("objdump"); err != nil {
+			detectedTool = "objdump"
+		} else {
+			detectedTool = "readelf"
+		}
+	})
+	return detectedTool
+}
+
 func CheckDuplicateSymbols(ctx context.Context, objFiles []string, verbose bool) error {
 	if len(objFiles) <= 1 {
 		return nil
@@ -25,32 +48,69 @@ func CheckDuplicateSymbols(ctx context.Context, objFiles []string, verbose bool)
 	if ctx == nil {
 		ctx = context.Background()
 	}
-	symbolMap := make(map[string][]SymbolInfo)
+
+	type result struct {
+		obj  string
+		syms []SymbolInfo
+		err  error
+	}
+
+	sem := make(chan struct{}, 16)
+	resultsChan := make(chan result, len(objFiles))
+	var wg sync.WaitGroup
+
 	for _, obj := range objFiles {
-		if err := utils.CheckFileExists(obj); err != nil {
-			return err
-		}
-		syms, err := readSymbols(ctx, obj, verbose)
-		if err != nil {
+		wg.Add(1)
+		go func(objFile string) {
+			defer wg.Done()
+			select {
+			case sem <- struct{}{}:
+				defer func() { <-sem }()
+			case <-ctx.Done():
+				resultsChan <- result{obj: objFile, err: ctx.Err()}
+				return
+			}
+
+			if err := utils.CheckFileExists(objFile); err != nil {
+				resultsChan <- result{obj: objFile, err: err}
+				return
+			}
+
+			syms, err := readSymbols(ctx, objFile, verbose)
+			resultsChan <- result{obj: objFile, syms: syms, err: err}
+		}(obj)
+	}
+
+	go func() {
+		wg.Wait()
+		close(resultsChan)
+	}()
+
+	symbolMap := make(map[string][]SymbolInfo)
+	for res := range resultsChan {
+		if res.err != nil {
 			if verbose {
-				fmt.Printf("Warning: cannot read symbols from %s: %v\n", obj, err)
+				fmt.Printf("Warning: cannot read symbols from %s: %v\n", res.obj, res.err)
 			}
 			continue
 		}
-		for _, sym := range syms {
+		for _, sym := range res.syms {
 			symbolMap[sym.Name] = append(symbolMap[sym.Name], sym)
 		}
 	}
+
 	duplicates := []string{}
 	for name, syms := range symbolMap {
 		if len(syms) > 1 && shouldCheckDuplicate(name) {
-			dup := fmt.Sprintf("symbol '%s' defined in:", name)
+			var files []string
 			for _, s := range syms {
-				dup += fmt.Sprintf(" %s", s.File)
+				files = append(files, fmt.Sprintf("%s (%s)", s.File, s.Type))
 			}
+			dup := fmt.Sprintf("  symbol '%s' defined in:\n    %s", name, strings.Join(files, "\n    "))
 			duplicates = append(duplicates, dup)
 		}
 	}
+
 	if len(duplicates) > 0 {
 		return fmt.Errorf("duplicate global symbols found:\n%s\nUse -no-symbol-check to skip this check", strings.Join(duplicates, "\n"))
 	}
@@ -68,13 +128,81 @@ func shouldCheckDuplicate(name string) bool {
 }
 
 func readSymbols(ctx context.Context, objPath string, verbose bool) ([]SymbolInfo, error) {
-	if _, err := exec.LookPath("nm"); err == nil {
-		return readSymbolsWithNm(ctx, objPath, verbose)
+	hash, err := utils.HashFile(objPath)
+	var cacheFile string
+	if err == nil {
+		if cacheDir, cerr := os.UserCacheDir(); cerr == nil {
+			cacheFile = filepath.Join(cacheDir, "fzt", "symbols", hash[:2], hash+".syms")
+
+			if data, rerr := os.ReadFile(cacheFile); rerr == nil {
+				if verbose {
+					fmt.Printf("Symbol cache hit for %s\n", objPath)
+				}
+				return deserializeSymbols(data, objPath), nil
+			}
+		}
 	}
-	if _, err := exec.LookPath("objdump"); err == nil {
-		return readSymbolsWithObjdump(ctx, objPath, verbose)
+
+	var syms []SymbolInfo
+	tool := getSymbolsTool()
+	switch tool {
+	case "nm":
+		syms, err = readSymbolsWithNm(ctx, objPath, verbose)
+	case "objdump":
+		syms, err = readSymbolsWithObjdump(ctx, objPath, verbose)
+	default:
+		syms, err = readSymbolsWithReadelf(ctx, objPath, verbose)
 	}
-	return readSymbolsWithReadelf(ctx, objPath, verbose)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if cacheFile != "" {
+		_ = os.MkdirAll(filepath.Dir(cacheFile), 0o755)
+		_ = os.WriteFile(cacheFile, serializeSymbols(syms), 0o644)
+	}
+
+	return syms, nil
+}
+
+func serializeSymbols(syms []SymbolInfo) []byte {
+	var sb strings.Builder
+	for _, s := range syms {
+		sb.WriteString(s.Name)
+		sb.WriteByte('\t')
+		sb.WriteString(s.Type)
+		sb.WriteByte('\t')
+		sb.WriteString(strconv.Itoa(s.Size))
+		sb.WriteByte('\t')
+		sb.WriteString(s.Bound)
+		sb.WriteByte('\n')
+	}
+	return []byte(sb.String())
+}
+
+func deserializeSymbols(data []byte, objPath string) []SymbolInfo {
+	var syms []SymbolInfo
+	scanner := bufio.NewScanner(bytes.NewReader(data))
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			continue
+		}
+		parts := strings.Split(line, "\t")
+		if len(parts) < 4 {
+			continue
+		}
+		size, _ := strconv.Atoi(parts[2])
+		syms = append(syms, SymbolInfo{
+			File:  objPath,
+			Name:  parts[0],
+			Type:  parts[1],
+			Size:  size,
+			Bound: parts[3],
+		})
+	}
+	return syms
 }
 
 func readSymbolsWithNm(ctx context.Context, objPath string, verbose bool) ([]SymbolInfo, error) {
