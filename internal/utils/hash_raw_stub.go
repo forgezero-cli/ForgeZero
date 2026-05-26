7c569c06da3f573b4a84f18b54f1ac43a7e3a8bc
alexvoste <meshoff@proton.me>
2026-05-26 23:19:40 +0000

    internal/utils: update fallback file hashing stubs for non-UNIX targets

--- internal/utils/hash_raw_stub.go
+++ internal/utils/hash_raw_stub.go
@@ -4,64 +4,64 @@
 package utils
 
 import (
-    "io"
-    "os"
+	"io"
+	"os"
 
-    fzvfs "fz/internal/fs"
-    "github.com/zeebo/blake3"
+	fzvfs "fz/internal/fs"
+	"github.com/zeebo/blake3"
 )
 
 func hashRawFileDigest(path string) ([32]byte, error) {
-    var out [32]byte
-    if fileSystem() != fzvfs.Default {
-        f, err := openVerified(path)
-        if err != nil {
-            return out, ErrHashOpen
-        }
-        hasher := hasherPool.Get().(*blake3.Hasher)
-        var buf [65536]byte
-        if _, err := io.CopyBuffer(hasher, f, buf[:]); err != nil {
-            hasher.Reset()
-            hasherPool.Put(hasher)
-            f.Close()
-            return out, err
-        }
-        if cerr := f.Close(); cerr != nil {
-            hasher.Reset()
-            hasherPool.Put(hasher)
-            return out, cerr
-        }
-        digest := hasher.Digest()
-        if _, err := digest.Read(out[:]); err != nil {
-            hasher.Reset()
-            hasherPool.Put(hasher)
-            return out, err
-        }
-        hasher.Reset()
-        hasherPool.Put(hasher)
-        return out, nil
-    }
+	var out [32]byte
+	if fileSystem() != fzvfs.Default {
+		f, err := openVerified(path)
+		if err != nil {
+			return out, ErrHashOpen
+		}
+		hasher := hasherPool.Get().(*blake3.Hasher)
+		var buf [65536]byte
+		if _, err := io.CopyBuffer(hasher, f, buf[:]); err != nil {
+			hasher.Reset()
+			hasherPool.Put(hasher)
+			f.Close()
+			return out, err
+		}
+		if cerr := f.Close(); cerr != nil {
+			hasher.Reset()
+			hasherPool.Put(hasher)
+			return out, cerr
+		}
+		digest := hasher.Digest()
+		if _, err := digest.Read(out[:]); err != nil {
+			hasher.Reset()
+			hasherPool.Put(hasher)
+			return out, err
+		}
+		hasher.Reset()
+		hasherPool.Put(hasher)
+		return out, nil
+	}
 
-    f, err := os.Open(path)
-    if err != nil {
-        return out, ErrHashOpen
-    }
-    defer f.Close()
+	f, err := os.Open(path)
+	if err != nil {
+		return out, ErrHashOpen
+	}
+	defer f.Close()
 
-    hasher := hasherPool.Get().(*blake3.Hasher)
-    var buf [65536]byte
-    if _, err := io.CopyBuffer(hasher, f, buf[:]); err != nil {
-        hasher.Reset()
-        hasherPool.Put(hasher)
-        return out, err
-    }
-    digest := hasher.Digest()
-    if _, err := digest.Read(out[:]); err != nil {
-        hasher.Reset()
-        hasherPool.Put(hasher)
-        return out, err
-    }
-    hasher.Reset()
-    hasherPool.Put(hasher)
-    return out, nil
+	hasher := hasherPool.Get().(*blake3.Hasher)
+	var buf [65536]byte
+	if _, err := io.CopyBuffer(hasher, f, buf[:]); err != nil {
+		hasher.Reset()
+		hasherPool.Put(hasher)
+		return out, err
+	}
+	digest := hasher.Digest()
+	if _, err := digest.Read(out[:]); err != nil {
+		hasher.Reset()
+		hasherPool.Put(hasher)
+		return out, err
+	}
+	hasher.Reset()
+	hasherPool.Put(hasher)
+	return out, nil
 }
