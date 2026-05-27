f8c0112c7e876273d08ecc72d47147a35e6da661
alexvoste <meshoff@proton.me>
2026-05-27 15:11:59 +0000

    refactor(main): completely remove CGO dependencies and import 'C' from entrypoint

--- cmd/fz/main.go
+++ cmd/fz/main.go
@@ -1,12 +1,5 @@
 package main
 
-/*
-#cgo CFLAGS: -I${SRCDIR}/../../internal/cplugin
-#include <stdlib.h>
-#include "fz_module.h"
-*/
-import "C"
-
 import (
 	"context"
 	"encoding/json"
@@ -1281,70 +1274,27 @@ func main() {
 			noCache = true
 		}
 	}
-
 	if pluginPath != "" {
-		var cctx C.fz_context_t
-		cPluginPath := C.CString(pluginPath)
-		cConfigPath := C.CString(configPath)
-		cSourcePath := C.CString(srcPath)
-		cDirPath := C.CString(dirPath)
-		cOutBin := C.CString(outBin)
-		cOutObj := C.CString(outObj)
-		cBuildType := C.CString(buildType)
-		cTarget := C.CString(target)
-		cToolchain := C.CString(toolchain)
-		cMode := C.CString(mode)
-		cCcFlags := C.CString(ccFlags)
-		cLdFlags := C.CString(ldFlags)
-		cFormat := C.CString(format)
-		cIsolation := C.CString(isolation)
-		defer C.free(unsafe.Pointer(cPluginPath))
-		defer C.free(unsafe.Pointer(cConfigPath))
-		defer C.free(unsafe.Pointer(cSourcePath))
-		defer C.free(unsafe.Pointer(cDirPath))
-		defer C.free(unsafe.Pointer(cOutBin))
-		defer C.free(unsafe.Pointer(cOutObj))
-		defer C.free(unsafe.Pointer(cBuildType))
-		defer C.free(unsafe.Pointer(cTarget))
-		defer C.free(unsafe.Pointer(cToolchain))
-		defer C.free(unsafe.Pointer(cMode))
-		defer C.free(unsafe.Pointer(cCcFlags))
-		defer C.free(unsafe.Pointer(cLdFlags))
-		defer C.free(unsafe.Pointer(cFormat))
-		defer C.free(unsafe.Pointer(cIsolation))
-		cctx.plugin_path = cPluginPath
-		cctx.config_path = cConfigPath
-		cctx.source_path = cSourcePath
-		cctx.dir_path = cDirPath
-		cctx.out_bin = cOutBin
-		cctx.out_obj = cOutObj
-		cctx.build_type = cBuildType
-		cctx.target = cTarget
-		cctx.toolchain = cToolchain
-		cctx.mode = cMode
-		cctx.cc_flags = cCcFlags
-		cctx.ld_flags = cLdFlags
-		cctx.format = cFormat
-		cctx.isolation = cIsolation
-		var cSourceDirs []*C.char
-		if dirPath != "" {
-			cSourceDirs = append(cSourceDirs, cDirPath)
+		goCtx := cplugin.GoContext{
+			PluginPath: pluginPath,
+			ConfigPath: configPath,
+			SourcePath: srcPath,
+			DirPath:    dirPath,
+			OutBin:     outBin,
+			OutObj:     outObj,
+			BuildType:  buildType,
+			Target:     target,
+			Toolchain:  toolchain,
+			Mode:       mode,
+			CcFlags:    ccFlags,
+			LdFlags:    ldFlags,
+			Format:     format,
+			Isolation:  isolation,
 		}
 		if cfg != nil {
-			for _, d := range cfg.SourceDirs {
-				cSourceDirs = append(cSourceDirs, C.CString(d))
-				defer C.free(unsafe.Pointer(cSourceDirs[len(cSourceDirs)-1]))
-			}
+			goCtx.SourceDirs = cfg.SourceDirs
 		}
-		if len(cSourceDirs) > 0 {
-			cctx.source_dir_count = C.int(len(cSourceDirs))
-			cctx.source_dirs = (**C.char)(C.malloc(C.size_t(len(cSourceDirs)) * C.size_t(unsafe.Sizeof(uintptr(0)))))
-			arr := (*[1 << 30]*C.char)(unsafe.Pointer(cctx.source_dirs))
-			for i, s := range cSourceDirs {
-				arr[i] = s
-			}
-			defer C.free(unsafe.Pointer(cctx.source_dirs))
-		}
+
 		m, err := cplugin.Load(pluginPath)
 		if err != nil {
 			writeStderr(err.Error())
@@ -1352,7 +1302,13 @@ func main() {
 			os.Exit(1)
 		}
 		defer m.Close()
-		if err := m.CallEntryBySymbol("fz_init_module", unsafe.Pointer(&cctx)); err != nil {
+		if err := m.CallInitWithGoContext(goCtx); err != nil {
+			writeStderr(err.Error())
+			writeStderr("\n")
+			os.Exit(1)
+		}
+		m, err = cplugin.Load(pluginPath)
+		if err != nil {
 			writeStderr(err.Error())
 			writeStderr("\n")
 			os.Exit(1)
