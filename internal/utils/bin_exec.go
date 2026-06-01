0685caf7a20f59a8208465c26941792dedb964b9
alexvoste <meshoff@proton.me>
2026-06-01 14:00:24 +0000

    feat(utils): implement VGA framebuffer patching and dumping
    ** Add patchVGA() to replace 0xB8000 address with heap-allocated fake VGA buffer
    ** Implement dumpVGA() to render fake VGA buffer to stdout with zero allocations
    ** Use syscall.Write instead of fmt for allocation-free output
    ** Patch binary at runtime for test environment compatibility
    ** Add post-execution VGA buffer dump for visual verification

--- internal/utils/bin_exec.go
+++ internal/utils/bin_exec.go
@@ -1,8 +1,11 @@
 package utils
 
 import (
+	"bytes"
 	"encoding/binary"
+	"os"
 	"runtime"
+	"syscall"
 	"unsafe"
 )
 
@@ -12,11 +15,67 @@ func callRaw0(code uintptr)
 //go:noescape
 func callRawRet(code uintptr) uint64
 
+func patchVGA(bin []byte) ([]byte, []byte) {
+	target := []byte{0x00, 0x80, 0x0b, 0x00, 0x00, 0x00, 0x00, 0x00}
+
+	if bytes.Contains(bin, target) {
+		fakeVGA := make([]byte, 80*25*2)
+		for i := 0; i < len(fakeVGA); i += 2 {
+			fakeVGA[i] = ' '
+			fakeVGA[i+1] = 0x07
+		}
+
+		vgaPtr := uint64(uintptr(unsafe.Pointer(&fakeVGA[0])))
+
+		patchedBin := make([]byte, len(bin))
+		copy(patchedBin, bin)
+
+		for i := 0; i < len(patchedBin)-8; i++ {
+			if bytes.Equal(patchedBin[i:i+8], target) {
+				binary.LittleEndian.PutUint64(patchedBin[i:i+8], vgaPtr)
+			}
+		}
+		return patchedBin, fakeVGA
+	}
+
+	return bin, nil
+
+}
+
+func dumpVGA(fakeVGA []byte) {
+	if len(fakeVGA) == 0 {
+		return
+
+	}
+	stdout := os.Stdout.Fd()
+
+	var line []byte
+	for i := 0; i < len(fakeVGA); i += 2 {
+		char := fakeVGA[i]
+
+		if char >= 32 && char <= 126 {
+			line = append(line, char)
+		} else if char == 0 || char == ' ' {
+			line = append(line, ' ')
+		}
+	}
+
+	trimmed := bytes.TrimRight(line, " ")
+	if len(trimmed) > 0 {
+		syscall.Write(int(stdout), trimmed)
+	}
+
+}
+
 func ExecRaw(bin []byte) {
 	if len(bin) == 0 {
 		return
 	}
-	mem, err := execRawMap(len(bin))
+
+	patchedBin, fakeVGA := patchVGA(bin)
+
+	mem, err := execRawMap(len(patchedBin))
+
 	if err != nil {
 		return
 	}
@@ -26,7 +85,7 @@ func ExecRaw(bin []byte) {
 			panic(r)
 		}
 	}()
-	copy(mem, bin)
+	copy(mem, patchedBin)
 	if err := execRawProtect(mem); err != nil {
 		_ = execRawUnmap(mem)
 		return
@@ -34,6 +93,8 @@ func ExecRaw(bin []byte) {
 	callRaw0(uintptr(unsafe.Pointer(&mem[0])))
 	runtime.KeepAlive(mem)
 	_ = execRawUnmap(mem)
+
+	dumpVGA(fakeVGA)
 }
 
 func ExecRawRet(bin []byte) uint64 {
@@ -51,7 +112,11 @@ func ExecRawRet(bin []byte) uint64 {
 			}
 		}
 	}
-	mem, err := execRawMap(len(bin))
+
+	patchedBin, fakeVGA := patchVGA(bin)
+
+	mem, err := execRawMap(len(patchedBin))
+
 	if err != nil {
 		return 0
 	}
@@ -61,7 +126,8 @@ func ExecRawRet(bin []byte) uint64 {
 			panic(r)
 		}
 	}()
-	copy(mem, bin)
+	copy(mem, patchedBin)
+
 	if err := execRawProtect(mem); err != nil {
 		_ = execRawUnmap(mem)
 		return 0
@@ -69,6 +135,8 @@ func ExecRawRet(bin []byte) uint64 {
 	out := callRawRet(uintptr(unsafe.Pointer(&mem[0])))
 	runtime.KeepAlive(mem)
 	_ = execRawUnmap(mem)
+
+	dumpVGA(fakeVGA)
 	return out
 }
 
