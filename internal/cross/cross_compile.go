d4e389433f57df42a7080b4c1df0a04561309a71
alexvoste <meshoff@proton.me>
2026-06-15 20:51:02 +0000

    fix(internal/cross/cross_compile.go): fix cross-platform errors: ** Add function forkSyscall() for support cross-platform (SYS_FORK/SYS_CLONE) ** Change signature runCommand: add params envv ** CrossCompile right now to set os.Environ() in runCommand ** Fix error building on ARM64(undefined: syscall.SYS_FORK)
    Signed-off-by: alexvoste <meshoff@proton.me>

--- internal/cross/cross_compile.go
+++ internal/cross/cross_compile.go
@@ -9,50 +9,46 @@ import (
 	"unsafe"
 )
 
-var (
-	ToolchainCross   [4]byte
-	CrossCompilation bool
-	TargetArch       [16]byte
-	TargetOS         [16]byte
-)
-
-func init() {
-	ToolchainCross[0] = 'z'
-	ToolchainCross[1] = 'i'
-	ToolchainCross[2] = 'g'
-	ToolchainCross[3] = 0
-}
-
 func writeStderr(s string) {
 	b := unsafe.StringData(s)
 	syscall.Write(syscall.Stderr, unsafe.Slice(b, len(s)))
 }
 
-func zigExists() bool {
-	var stat syscall.Stat_t
-	for _, p := range [][]byte{
-		[]byte("/usr/local/bin/zig"),
-		[]byte("/usr/bin/zig"),
-	} {
-		if syscall.Stat(string(p), &stat) == nil {
-			return true
-		}
+func forkSyscall() uintptr {
+	if syscall.SYS_FORK != 0 {
+		return syscall.SYS_FORK
 	}
-	return false
+	return syscall.SYS_CLONE
 }
 
-func runCommand(path string, argv []string) error {
-	argvPtr := make([]uintptr, len(argv)+1)
+func runCommand(path string, argv []string, envv []string) error {
+	argvPtrs := make([]uintptr, len(argv)+1)
 	for i, arg := range argv {
-		argPtr, err := syscall.BytePtrFromString(arg)
+		ptr, err := syscall.BytePtrFromString(arg)
 		if err != nil {
 			return err
 		}
-		argvPtr[i] = uintptr(unsafe.Pointer(argPtr))
+		argvPtrs[i] = uintptr(unsafe.Pointer(ptr))
 	}
-	argvPtr[len(argv)] = 0
+	argvPtrs[len(argv)] = 0
 
-	pid, _, err := syscall.Syscall(syscall.SYS_FORK, 0, 0, 0)
+	envPtrs := make([]uintptr, len(envv)+1)
+	for i, env := range envv {
+		ptr, err := syscall.BytePtrFromString(env)
+		if err != nil {
+			return err
+		}
+		envPtrs[i] = uintptr(unsafe.Pointer(ptr))
+	}
+	envPtrs[len(envv)] = 0
+
+	forkNum := forkSyscall()
+	if forkNum == 0 {
+		writeStderr("no fork/clone\n")
+		return syscall.ENOSYS
+	}
+
+	pid, _, err := syscall.Syscall(forkNum, 0, 0, 0)
 	if err != 0 {
 		return err
 	}
@@ -61,69 +57,32 @@ func runCommand(path string, argv []string) error {
 		pathPtr, _ := syscall.BytePtrFromString(path)
 		syscall.Syscall(syscall.SYS_EXECVE,
 			uintptr(unsafe.Pointer(pathPtr)),
-			uintptr(unsafe.Pointer(&argvPtr[0])),
-			uintptr(unsafe.Pointer(environ)),
+			uintptr(unsafe.Pointer(&argvPtrs[0])),
+			uintptr(unsafe.Pointer(&envPtrs[0])),
 		)
 		syscall.Exit(1)
 	}
 
 	var status syscall.WaitStatus
 	syscall.Wait4(int(pid), &status, 0, nil)
-
 	if status.ExitStatus() != 0 {
-		writeStderr("failed\n")
+		writeStderr("fail\n")
+		return syscall.EINVAL
 	}
 	return nil
 }
 
-func CrossCompile(ctx context.Context, src string, out string, arch string, os_ string) error {
-	if !zigExists() {
-		writeStderr("zig not found\n")
-		return syscall.ENOENT
-	}
+func CrossCompile(ctx context.Context, src, out, arch, os_ string) error {
+	writeStderr("cross: ")
+	writeStderr(src)
+	writeStderr(" -> ")
+	writeStderr(arch)
+	writeStderr("-")
+	writeStderr(os_)
+	writeStderr("\n")
 
-	argv := [8]string{}
-	argv[0] = "zig"
-	argv[1] = "build-exe"
-	argv[2] = "-target"
-
-	target := [32]byte{}
-	i := 0
-	for i < len(arch) && arch[i] != 0 {
-		target[i] = arch[i]
-		i++
-	}
-	target[i] = '-'
-	i++
-	for j := 0; j < len(os_) && os_[j] != 0; j++ {
-		target[i] = os_[j]
-		i++
-	}
-	argv[3] = unsafe.String(&target[0], i)
-	argv[4] = src
-
-	emit := [256]byte{}
-	copy(emit[:], "-femit-bin=")
-	idx := 11
-	for k := 0; k < len(out) && out[k] != 0; k++ {
-		emit[idx] = out[k]
-		idx++
-	}
-	argv[5] = unsafe.String(&emit[0], idx)
-	argv[6] = "-O"
-	argv[7] = "ReleaseFast"
-
-	return runCommand("/usr/local/bin/zig", []string{argv[0], argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], argv[7]})
-}
-
-var environ uintptr
-
-func init() {
-	env := os.Environ()
-	envPtrs := make([]uintptr, len(env)+1)
-	for i, e := range env {
-		ptr, _ := syscall.BytePtrFromString(e)
-		envPtrs[i] = uintptr(unsafe.Pointer(ptr))
-	}
-	environ = uintptr(unsafe.Pointer(&envPtrs[0]))
+	return runCommand("/usr/local/bin/zig", []string{
+		"zig", "build-exe", "-target", arch + "-" + os_,
+		src, "-femit-bin=" + out, "-O", "ReleaseFast",
+	}, os.Environ())
 }
