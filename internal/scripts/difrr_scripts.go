e557ed8336a47653c1e88793987c375e1a06c977
alexvoste <alexvostedev@proton.me>
2026-06-24 13:25:38 +0000

    fix(scripts): remove verbose output and pass stdout/stderr directly
    - Remove duplicate output of command string
    - Use os.Stdout/os.Stderr directly in runCommand
    - Simplify execution logic
    
    Signed-off-by: alexvoste <alexvostedev@proton.me>

--- internal/scripts/difrr_scripts.go
+++ internal/scripts/difrr_scripts.go
@@ -18,12 +18,7 @@ type ScriptsConfigure struct {
 
 func (s *ScriptsConfigure) Run(ctx context.Context) error {
 	for _, cmd := range s.Commands {
-		if s.Verbose {
-			os.Stdout.WriteString("Running script: ")
-			os.Stdout.WriteString(cmd)
-			os.Stdout.WriteString("\n")
-		}
-		_, err := runCommand(ctx, s.Verbose, os.Stdout, os.Stderr, "sh", "-c", cmd)
+		_, err := runCommand(ctx, false, os.Stdout, os.Stderr, "sh", "-c", cmd)
 		if err != nil {
 			return err
 		}
