ab3b7ce9616d0f08273a684be635ad51f69eef22
alexvoste <alexvostedev@proton.me>
2026-06-29 19:54:15 +0000

    style: fix comment block formatting to comply with copyright header
    Converts multiline comment to triple-quoted string for consistency.
    
    Signed-off-by: alexvoste <alexvostedev@proton.me>

--- internal/pyzero/parser.py
+++ internal/pyzero/parser.py
@@ -1,21 +1,19 @@
-'''
- *   Copyright (c) 2026 ForgeZero-cli
- *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
-'''
-
-
+"""
+*   Copyright (c) 2026 ForgeZero-cli
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 
 import ast
 import json
@@ -73,4 +71,3 @@ if __name__ == "__main__":
     generator.visit(tree)
 
     print(json.dumps(generator.instructions, indent=2))
-
