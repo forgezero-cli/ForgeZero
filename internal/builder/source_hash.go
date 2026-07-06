df563f5175bce6120ecd3928e456b747a1f654b7
alexvoste <alexvostedev@proton.me>
2026-07-06 12:48:32 +0000

    builder: implement source file hashing and refresh logic using BLAKE3
    Add functions to compute BLAKE3 hashes of source files (via the
    github.com/zeebo/blake3 library) and to refresh the global sourceHashes
    map by walking the given directories and reading each file. The refresh
    process (refreshSourceHashes) is called at the beginning of each build
    to capture the current state of the source tree.
    
    The hash is used to compare against the stored cache; if a file's
    hash matches, the builder can reuse the previously compiled object.
    BLAKE3 was chosen for its high performance and cryptographic security,
    making it ideal for fast incremental builds. The implementation uses
    a 32 KiB buffer for efficient file reading and handles errors gracefully,
    skipping unreadable files and logging warnings to avoid build failures.
    
    Signed-off-by: alexvoste <alexvostedev@proton.me>

---
+++ internal/builder/source_hash.go
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2026 ForgeZero-cli
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package builder
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/zeebo/blake3"
+)
+
+var sourceHashes = make(map[string][32]byte)
+
+func refreshSourceHashes(dirs []string) error {
+	buf := make([]byte, 32*1024)
+	sourceHashes = make(map[string][32]byte)
+	for _, root := range dirs {
+		if root == "" {
+			continue
+		}
+		if err := filepath.WalkDir(root, func(path string, d os.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+			if d.IsDir() {
+				return nil
+			}
+			f, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			h := blake3.New()
+			for {
+				n, rerr := f.Read(buf)
+				if n > 0 {
+					_, _ = h.Write(buf[:n])
+				}
+				if rerr == io.EOF {
+					break
+				}
+				if rerr != nil {
+					_ = f.Close()
+					return rerr
+				}
+			}
+			_ = f.Close()
+			var sum [32]byte
+			h.Sum(sum[:0])
+			sourceHashes[path] = sum
+			return nil
+		}); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
