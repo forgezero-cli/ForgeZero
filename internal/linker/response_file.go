e6a42113fe6137765d4e367e2f16fa480f82c58e
alexvoste <alexvostedev@proton.me>
2026-07-06 12:47:39 +0000

    linker: extract response‑file creation logic to dedicated module
    Move the shouldUseResponseFile and createResponseFile functions from
    link.go into this new file. The logic is unchanged, but now it is
    self‑contained, making it easier to maintain and test. The module
    exports functions to determine whether a response file is needed
    (based on argument count and total length) and to create a temporary
    response file with the given arguments.
    
    This refactoring improves code organisation and paves the way for
    future enhancements, such as supporting different response file
    formats or integrating with external tools.
    
    Signed-off-by: alexvoste <alexvostedev@proton.me>

---
+++ internal/linker/response_file.go
@@ -0,0 +1,95 @@
+/*
+ *   Copyright (c) 2026 ForgeZero-cli
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version of the License.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package linker
+
+import (
+	"bufio"
+	"errors"
+	"os"
+	"strings"
+
+	"github.com/forgezero-cli/ForgeZero/internal/utils"
+)
+
+const (
+    maxResponseFileBytes = 32 * 1024
+    maxResponseFileArg   = 8192
+)
+
+func shouldUseResponseFile(args []string) bool {
+    if len(args) > 128 {
+        return true
+    }
+    total := 0
+    for _, arg := range args {
+        if len(arg) > maxResponseFileArg {
+            return true
+        }
+        total += len(arg) + 1
+        if total > maxResponseFileBytes {
+            return true
+        }
+    }
+    return false
+}
+
+func createResponseFile(args []string) (string, error) {
+    f, err := os.CreateTemp("", "fz_link_args_*.rsp")
+    if err != nil {
+        return "", err
+    }
+    name := f.Name()
+    if err := f.Chmod(utils.FilePerm); err != nil {
+        f.Close()
+        os.Remove(name)
+        return "", err
+    }
+    writer := bufio.NewWriterSize(f, 64*1024)
+    for _, arg := range args {
+        if strings.ContainsAny(arg, "\n\r\x00") {
+            f.Close()
+            os.Remove(name)
+            return "", errors.New("invalid argument for response file")
+        }
+        if err := utils.ValidateCLIArg(arg); err != nil {
+            f.Close()
+            os.Remove(name)
+            return "", errors.New("invalid argument for response file: " + err.Error())
+        }
+        if _, err := writer.WriteString(arg); err != nil {
+            f.Close()
+            os.Remove(name)
+            return "", err
+        }
+        if err := writer.WriteByte('\n'); err != nil {
+            f.Close()
+            os.Remove(name)
+            return "", err
+        }
+    }
+    if err := writer.Flush(); err != nil {
+        f.Close()
+        os.Remove(name)
+        return "", err
+    }
+    if err := f.Close(); err != nil {
+        os.Remove(name)
+        return "", err
+    }
+    return name, nil
+}
