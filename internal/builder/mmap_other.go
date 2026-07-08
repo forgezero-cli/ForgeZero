5e53c67c93e44411840c8ce373a4e7393971f6f4
alexvoste <alexvostedev@proton.me>
2026-07-08 20:33:07 +0000

    fix(builder): correct license header and add stub for prefetchMappedFile
    Signed-off-by: alexvoste <alexvostedev@proton.me>

--- internal/builder/mmap_other.go
+++ internal/builder/mmap_other.go
@@ -15,7 +15,7 @@
  *   GNU General Public License for more details.
  *
  *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/\>.
  */
 
 package builder
@@ -29,3 +29,5 @@ func mmapFile(fd int, size int) ([]byte, error) {
 func munmapFile(data []byte) error {
 	return nil
 }
+
+func prefetchMappedFile(data []byte) {}
