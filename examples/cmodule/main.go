c7b34618c7d40bbba2ef0ce006aa473dbd9a7271
alexvoste <alexvostedev@proton.me>
2026-07-15 13:11:55 +0000

    ref(cmodule): delete using standart fmt and add forgezero-cli copyright
    Signed-off-by: alexvoste <alexvostedev@proton.me>

--- examples/cmodule/main.go
+++ examples/cmodule/main.go
@@ -1,11 +1,26 @@
-// !SPDX-License-Identifier: MIT
+/*
+ * Copyright (c) 2026 ForgeZero-cli
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 package main
 
 import (
-	"fmt"
 	"unsafe"
 
+	"github.com/forgezero-cli/ForgeZero/cmd/fz/stdio"
 	"github.com/forgezero-cli/ForgeZero/internal/cplugin"
 )
 
@@ -19,5 +34,5 @@ func main() {
 	if err := m.CallEntryBySymbol("fz_init_module", unsafe.Pointer(&ctxValue)); err != nil {
 		panic(err)
 	}
-	fmt.Println(ctxValue)
+	stdio.WriteFmt(2, "", ctxValue)
 }
