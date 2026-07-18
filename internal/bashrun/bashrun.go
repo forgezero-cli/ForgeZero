bf06eb880dafe4e01e7fa553fa11d9dc688945ab
alexvoste <alexvostedev@proton.me>
2026-07-18 12:33:01 +0000

    chore: replace deprecated ioutil.TempFile
    Signed-off-by: alexvoste <alexvostedev@proton.me>

--- internal/bashrun/bashrun.go
+++ internal/bashrun/bashrun.go
@@ -1,9 +1,23 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package bashrun
 
 import (
 	"context"
 	"errors"
-	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -27,7 +41,7 @@ func RunInline(ctx context.Context, script string, verbose bool) error {
 }
 
 func runWithShell(ctx context.Context, shellPath, script string, verbose bool) error {
-	tmp, err := ioutil.TempFile("", "fz-script-*.sh")
+	tmp, err := os.CreateTemp("", "fz-script-*.sh")
 	if err != nil {
 		return err
 	}
