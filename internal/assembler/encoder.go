1199697801600517086a32136e4e203803621a49
alexvoste <alexvostedev@proton.me>
2026-07-18 12:33:01 +0000

    perf: optimize Encoder with capacity checks and larger buffer
    Signed-off-by: alexvoste <alexvostedev@proton.me>

--- internal/assembler/encoder.go
+++ internal/assembler/encoder.go
@@ -17,36 +17,99 @@
 
 package assembler
 
-import "sync"
+import (
+	"sync"
+)
 
 type Encoder struct {
-	buf []byte
+	buf  []byte
+	next *Encoder
 }
 
-var encPool = sync.Pool{
-	New: func() any {
-		b := make([]byte, 0, 256)
+var encPool sync.Pool
+
+func init() {
+	encPool.New = func() any {
+		b := make([]byte, 0, 4096)
 		return &Encoder{buf: b}
-	},
+	}
 }
 
 func GetEncoder() *Encoder {
-	e := encPool.Get().(*Encoder)
+	v := encPool.Get()
+	if v == nil {
+		b := make([]byte, 0, 4096)
+		return &Encoder{buf: b}
+	}
+	e := v.(*Encoder)
+	e.next = nil
 	e.buf = e.buf[:0]
 	return e
 }
 
+func (e *Encoder) Reserve(n int) {
+	free := cap(e.buf) - len(e.buf)
+	if free >= n {
+		return
+	}
+	need := len(e.buf) + n
+	newCap := cap(e.buf) * 2
+	if newCap < 4096 {
+		newCap = 4096
+	}
+	if newCap < need {
+		newCap = need
+	}
+	nb := make([]byte, len(e.buf), newCap)
+	copy(nb, e.buf)
+	e.buf = nb
+}
+
 func PutEncoder(e *Encoder) {
+	e.buf = e.buf[:0]
+	e.next = nil
 	encPool.Put(e)
 }
 
 func (e *Encoder) WriteByte(b byte) error {
-	e.buf = append(e.buf, b)
+	n := len(e.buf)
+	if n+1 <= cap(e.buf) {
+		e.buf = e.buf[:n+1]
+		e.buf[n] = b
+		return nil
+	}
+	writeByteGrow(e, b)
 	return nil
 }
 
 func (e *Encoder) Write(p []byte) {
-	e.buf = append(e.buf, p...)
+	n := len(e.buf)
+	need := n + len(p)
+	if need <= cap(e.buf) {
+		e.buf = e.buf[:need]
+		copy(e.buf[n:], p)
+		return
+	}
+	newCap := cap(e.buf) * 2
+	if newCap < 256 {
+		newCap = 256
+	}
+	if newCap < need {
+		newCap = need
+	}
+	nb := make([]byte, n, newCap)
+	copy(nb, e.buf)
+	nb = nb[:need]
+	copy(nb[n:], p)
+	e.buf = nb
+}
+
+func writeByteGrow(e *Encoder, b byte) {
+	e.buf = append(e.buf, b)
+}
+
+func writeByteAsm(e *Encoder, b byte) {
+	writeByteGrow(e, b)
 }
 
 func (e *Encoder) Bytes() []byte { return e.buf }
